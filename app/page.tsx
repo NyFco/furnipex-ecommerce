95a7bce80d5aa7e51966a9cdebe5e8e569e2181e
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2024-01-21 12:47:29 +0330

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import HeroSection from "./components/heroSection";
 import "./styles/globals.scss";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <HeroSection />
     </main>
   );
 }
