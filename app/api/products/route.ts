19d6d1f0f98e26956b37475fc71ce7ee4d7ed1b9
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2024-01-28 18:15:09 +0330

    update api

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextApiRequest } from "next";
 import data from "./data.json";
 
-export async function GET() {
-  return NextResponse.json(data);
+export async function GET(req: NextApiRequest) {
+  const { searchParams } = new URL(req.url as any);
+  const id = searchParams.get("id");
+
+  if (id) {
+    const productId = typeof id === "string" ? parseInt(id, 10) : id;
+    const foundItem = data.find((item) => item.id === productId);
+
+    if (!foundItem) {
+      return Response.json({}, { status: 404 });
+    }
+
+    return Response.json([foundItem]);
+  }
+
+  return Response.json(data);
 }
